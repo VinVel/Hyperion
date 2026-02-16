db9ca6fd3cb8283f20d7af8c71eb88ef867623bf
git <git@velcore.net>
2026-02-16 16:31:43 +0100

    Create global AppScope

---
+++ composeApp/src/commonMain/kotlin/net/velcore/hyperion_client/scope/AppScope.kt
@@ -0,0 +1,16 @@
+package net.velcore.hyperion_client.scope
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancelAndJoin
+
+class AppScope {
+    private val supervisorJob = SupervisorJob()
+
+    val scope: CoroutineScope = CoroutineScope(supervisorJob + Dispatchers.Default)
+
+    suspend fun shutdown() {
+        supervisorJob.cancelAndJoin()
+    }
+}

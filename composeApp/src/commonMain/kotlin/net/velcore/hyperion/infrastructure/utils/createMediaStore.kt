a03e875a4d7016a2ff500277fb219321ff59dbf5
git <git@velcore.net>
2026-02-19 22:54:12 +0100

    Try a different approach at getting the needed plattform specific dependencies

---
+++ composeApp/src/commonMain/kotlin/net/velcore/hyperion/infrastructure/utils/createMediaStore.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2026 VinVel
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, version 3.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Project home: hyperion.velcore.net
+ */
+
+package net.velcore.hyperion.infrastructure.utils
+
+import de.connect2x.trixnity.client.media.MediaStore
+
+expect suspend fun createMediaStore(path: String): MediaStore

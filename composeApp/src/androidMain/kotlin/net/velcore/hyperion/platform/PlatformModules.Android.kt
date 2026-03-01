08d38f521f12294caf10a0c321aef0a6dfa4e15c
git <git@velcore.net>
2026-03-01 18:10:15 +0100

    Delete old KMP Project

--- composeApp/src/androidMain/kotlin/net/velcore/hyperion/platform/PlatformModules.Android.kt
+++
@@ -1,2 +0,0 @@
-package net.velcore.hyperion.platform
-
--- composeApp/src/iosMain/kotlin/net/velcore/hyperion/MainViewController.kt
+++
@@ -1,5 +0,0 @@
-package net.velcore.hyperion
-
-import androidx.compose.ui.window.ComposeUIViewController
-
-fun MainViewController() = ComposeUIViewController { App() }

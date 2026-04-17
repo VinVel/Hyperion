5fe96c85fb7f1d1d17190a220a828f8d029c55b7
VinVel05 <git@velcore.net>
2026-04-17 22:57:53 +0200

    Add missing file level license headers (again)

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2026 VinVel
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, version 3 only.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Project home: hyperion.velcore.net
+ */
+
 fn main() {
     tauri_build::build()
 }
